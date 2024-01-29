448af3494a60c688d41723c832801e57cd5448fd
abudgethuman <146788429+abudgethuman@users.noreply.github.com>
2024-01-29 05:31:06 +0000

    Update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Password-Generator
 A simple password generator made in python
+
+**Usage**
+
+Install libs:
+```bash
+pip install -r requirements.txt
+```
+
+Run the script:
+```bash
+python3 main.py
+```
+
+**__Get some good passwords!__**
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 CHARACTER_SETS = {
     '1': "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -11,27 +14,27 @@ DEFAULT_LENGTH = 8
 def pass_gen(length, char_types):
     selected_chars = ''.join(CHARACTER_SETS[char_type] for char_type in char_types)
     result = ''.join(random.choice(selected_chars) for _ in range(length))
-    print(f"\nYour password is: {result}")
+
+    print(f"\nYour password is: {Fore.GREEN}{result}")
 
 def get_valid_length():
     while True:
-        length_str = input("Enter password length: ")
-        if not length_str: 
+        length_str = input(f"{Fore.CYAN}Enter password length (press Enter for default length): ")
+        if not length_str:
             return DEFAULT_LENGTH
         try:
             length = int(length_str)
             if length > 0:
                 return length
             else:
-                print("Please enter a positive integer for the password length.")
+                print(f"{Fore.RED}Please enter a positive integer for the password length.")
         except ValueError:
-            print("Invalid input. Please enter a valid number for the password length.")
-
+            print(f"{Fore.RED}Invalid input. Please enter a valid number for the password length.")
 
 def get_valid_char_types():
     allowed_char_types = set("1234")
     while True:
-        chars_to_include = input("""\n
+        chars_to_include = input(f"""{Fore.CYAN}\n
 Characters to include in your password:
 =======================================
 [1] Uppercase  [3] Numbers
@@ -42,7 +45,7 @@ Enter the numbers of the types you want (e.g., "1234"): """)
         if all(char_type in allowed_char_types for char_type in chars_to_include):
             return chars_to_include
         else:
-            print("Invalid input. Please enter valid types.")
+            print(f"{Fore.RED}Invalid input. Please enter valid types.")
 
 length = get_valid_length()
 chars_to_include = get_valid_char_types()
